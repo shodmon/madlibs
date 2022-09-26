6cd5f2aac16dbd4f0ac532b2e65ef6f979aa1ba5
shodmon <Sbabakhanzoda@gmail.com>
2022-09-26 15:02:19 -0400

    Madlibs

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+print("Type 3 words")
+adj = input ("Adjective: ")
+verb = input("Verb: ")
+noun = input ("Noun: ")
+
+madlib = f"Throw {noun} into a lake. The effect will be not only visible but also far more lasting.\n\
+The {noun} will disrupt the still waters. A circle will form where the {noun} hit the water, and in a\n\
+flash that circle will multiply into another, then another. \n\
+If a {noun} hits a river, the river will treat it as yet another commotion in its already tumultuous\n\
+course. Nothing {adj}. Nothing unmanageable.\n\n\
+And if you {verb}, the universe will \"{verb}\" back.\n\n:)"
+
+print (madlib)
